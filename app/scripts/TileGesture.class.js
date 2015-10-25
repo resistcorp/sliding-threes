89d3c5f985c5e5569eac1ff7795ea9b0c10294de
Boris Evieux <boris@resistcorp.org>
2015-10-26 00:48:02 +0100

    refactoring

---
+++ app/scripts/TileGesture.class.js
@@ -0,0 +1,17 @@
+function TileGesture(){
+	
+}
+TileGesture.prototype = {
+	update : function(){
+		
+	},
+	updateWithTile : function(){
+		
+	},
+	updateWithDirection : function(){
+
+	}
+}
+TileGesture.CLICK = "click";
+TileGesture.TOUCH = "touch";
+TileGesture.KEYBOARD = "keyboard";
